f078a5e940f2586ba57421957a97c62ded43f2bf
Alex Lelievre <alex@folabs.com>
2020-01-17 17:27:03 -0800

    Initial Commit

---
+++ calcalc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  calcalc
+//
+//  Created by Alex Lelievre on 1/17/20.
+//  Copyright © 2020 Alex Lelievre. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
